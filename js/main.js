9c02567dc5c53ce6ea29e821ad627dbd2439b5fa
seval2016 <sevalsenturk2016@gmail.com>
2026-01-19 20:28:23 +0300

    page design

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
-function loadSection(id, file) {
+function loadSection(id, file, callback) {
   fetch(file)
     .then(res => res.text())
     .then(html => {
       document.getElementById(id).innerHTML = html;
+
+      // Eğer callback varsa çalıştır
+      if (callback) callback();
     });
 }
 
-loadSection("header", "partials/header.html");
+/* =================
+   SEARCH INIT
+================= */
+
+function initSearch() {
+
+  const toggle = document.getElementById("searchToggle");
+  const panel = document.getElementById("searchPanel");
+  const closeBtn = document.getElementById("closeSearch");
+
+  if (!toggle || !panel || !closeBtn) return;
+
+  toggle.addEventListener("click", () => {
+    panel.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    panel.classList.remove("active");
+  });
+}
+
+
+/* =================
+   LOAD PARTIALS
+================= */
+
+loadSection("header", "partials/header.html", initSearch);
 loadSection("hero", "partials/hero.html");
 loadSection("about", "partials/about.html");
 loadSection("publications", "partials/publications.html");
